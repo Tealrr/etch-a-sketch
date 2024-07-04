b33aca36038005a45e643c40a951025d9688975f
Tyler Rodrigues <tyler.r.rodrigues@gmail.com>
2024-07-03 20:21:29 -0700

    Created a button that allows you to change the grid size

--- javascript.js
+++ javascript.js
@@ -1,6 +1,21 @@
 const container = document.querySelector("#container")
 
+const setGridSizeButton = document.createElement("button");
+setGridSizeButton.id = "set-grid-size";
+setGridSizeButton.textContent = "Set Grid Size"
+
+const gridSizeInput = document.createElement("input");
+gridSizeInput.type = "number";
+gridSizeInput.id = "grid-size";
+gridSizeInput.min = 1;
+gridSizeInput.max = 100;
+gridSizeInput.value = 16;
+
+document.body.insertBefore(gridSizeInput, container);
+document.body.insertBefore(setGridSizeButton, container);
+
 function makeGrid(size) {
+    container.innerHTML = '';
     for (let i=0; i< size * size; i++) {
         const square = document.createElement("div")
         square.classList.add("square");
@@ -15,8 +30,10 @@ function makeGrid(size) {
     }
 }
 
-makeGrid(16)
 
-function hoverEffect() {
-    
-}
+setGridSizeButton.addEventListener("click", () => {
+    const newSize = gridSizeInput.value;
+    makeGrid(newSize);
+});
+
+makeGrid(16);
